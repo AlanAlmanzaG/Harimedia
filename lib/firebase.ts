1ebbe24ead0dbdf78f41462dcd533a52a963b1ad
Alan <alan037w7@gmail.com>
2026-02-27 12:48:28 -0600

    Firebase initialized

---
+++ lib/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrVnVkIOWljf4AalXiJjxoDUi9hLfQpP4",
+  authDomain: "harimedia-a869b.firebaseapp.com",
+  projectId: "harimedia-a869b",
+  storageBucket: "harimedia-a869b.firebasestorage.app",
+  messagingSenderId: "1078015149504",
+  appId: "1:1078015149504:web:f83e180aab6a77b74f7425"
+};
+
+export const app = initializeApp(firebaseConfig);
